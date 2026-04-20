3ac603d7fa64ffa3d70153a007bc45af2e9bdfe4
dynastygunawan22 <dynastygunawan22@gmail.com>
2026-04-21 01:03:18 +0700

    first commit

---
+++ DataMahasiswa103082400037.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpmodul7_Kelompok06_103082400037
+{
+    internal class DataMahasiswa103082400037
+    {
+    }
+}
